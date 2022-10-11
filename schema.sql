b1fa4951394ed05f9ac91bf750f9fdeb5d4c3b05
Skyler <103390952+SkylerDixon@users.noreply.github.com>
2022-10-10 21:47:30 -0700

    Uploaded new schema.sql
    from 7.2.2 with corrections to code

--- schema.sql
+++ schema.sql
@@ -39,7 +39,7 @@ Create TABLE salaries (
 
 CREATE TABLE dept_employee (
 	emp_no INT NOT NULL,  
-	dept_no INT NOT NULL, 
+	dept_no VARCHAR NOT NULL, 
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -52,8 +52,8 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-		PRIMARY KEY (emp_no) 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+		PRIMARY KEY (emp_no)
 );
 
 SELECT * FROM departments;
